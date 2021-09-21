63e1830fb7e3685053b87ab81d41e8fb83f6aafa
Eduardo <elanderos318@gmail.com>
2021-09-21 14:25:01 -0500

    add video-streaming.tf

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -16,3 +16,8 @@ provider "kubernetes" {
     client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
 }
+
+provider "null" {
+    version = "2.1.2"  
+}
+
